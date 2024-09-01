617d1c37329305c2cd92821c57f2e7f163ed0637
Kautilyahari <harikautilya@gmail.com>
2024-09-01 13:11:51 +0000

    New app created

---
+++ apps/2_scene/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+function App() {
+
+  return (
+    <div style={{
+      width: '100vw',
+      height: "100vh",
+    }}>
+      Alive
+    </div >
+  )
+}
+
+export default App;
